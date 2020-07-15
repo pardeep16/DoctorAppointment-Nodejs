70b6b5c8ee6414bb9c7cb3ca8d130978bf7ca427
Pardeep <pk.raswant@gmail.com>
2020-07-15 18:49:13 +0530

    Added codebase

--- controllers/signin.js
+++ controllers/signin.js
@@ -76,6 +76,7 @@ var addPhoneNumber=function(req,res){
 				console.log(result11);
 				var result=result11.Status;
 				console.log("result "+result);
+				console.log("check condition ",result==='Success');
 				if(result==='Success'){
 					console.log("Inside method");
 					signin.addNumberModel(data,function(errorr,results){
